00622470e0a3a4f46fbfab193436ff1a790daa44
Dragos Mocanasu <stilldragos10@gmail.com>
2023-04-03 21:40:04 +0200

    Update testAPIRasberryPI.sh

--- bash/testAPIRasberryPI.sh
+++ bash/testAPIRasberryPI.sh
@@ -18,11 +18,11 @@
 							sleep 60
 					done < $file
 					echo "\n--------------------------------: j:"$j
+			                echo $j"[$(date)]" >> $LOGFILE		
 				done
 				echo "\n================================: i:"$i 
 				sleep 300
 			done
-			echo "[$(date)]" >> $LOGFILE		
 		done
 
 	}
@@ -34,10 +34,10 @@
 
 	# Express
 	sleep 300
-	run_files "executables/express/employees.txt"
-	run_files "executables/express/departments.txt"
+	# run_files "executables/express/employees.txt"
+	# run_files "executables/express/departments.txt"
 	# Nest
-	run_files "executables/nest/employees.txt"
+	# run_files "executables/nest/employees.txt"
 	run_files "executables/nest/departments.txt"
 	# Fastify
 	run_files "executables/fastify/employees.txt"
