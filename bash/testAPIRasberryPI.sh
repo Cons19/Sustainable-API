be5b20af1faa2c30a1138f13e3e944b47295c8b9
Tarau Constantin Razvan <33446700+Cons19@users.noreply.github.com>
2023-04-27 13:20:28 +0200

    refactor: 💡 (bash) added extra 30 runs on for

--- bash/testAPIRasberryPI.sh
+++ bash/testAPIRasberryPI.sh
@@ -12,19 +12,22 @@
 			do
 				for j in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 				do
-					while IFS= read -r line; do
-							echo "\nCurl command: curl" $line
-							curl $line
-							sleep 60
-					done < $file
+					echo $j"[$(date)] - before" >> $LOGFILE
+					for k in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+					do
+						while IFS= read -r line; do
+								echo "\nCurl command: curl" $line
+								curl $line
+						done < $file
+					done
 					echo "\n--------------------------------: j:"$j
-			                echo $j"[$(date)]" >> $LOGFILE		
+			    echo $j"[$(date)] - after" >> $LOGFILE
+					sleep 60
 				done
 				echo "\n================================: i:"$i 
 				sleep 300
 			done
 		done
-
 	}
 
 	rm -f $LOGFILE
@@ -33,18 +36,18 @@
 	echo "[$(date)]" >> $LOGFILE
 
 	# Express
-	sleep 300
-	# run_files "executables/express/employees.txt"
+	# sleep 300
+	run_files "executables/express/employees.txt"
 	# run_files "executables/express/departments.txt"
 	# Nest
 	# run_files "executables/nest/employees.txt"
-	run_files "executables/nest/departments.txt"
+	# run_files "executables/nest/departments.txt"
 	# Fastify
-	run_files "executables/fastify/employees.txt"
-	run_files "executables/fastify/departments.txt"
+	# run_files "executables/fastify/employees.txt"
+	# run_files "executables/fastify/departments.txt"
 	# Koa
-	run_files "executables/koa/employees.txt"
-	run_files "executables/koa/departments.txt"
+	# run_files "executables/koa/employees.txt"
+	# run_files "executables/koa/departments.txt"
 
 	echo "done"
 	cat $LOGFILE
